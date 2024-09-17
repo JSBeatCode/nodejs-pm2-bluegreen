bb33a1520527528f6ba9b592afe10f51b27e1079
JS <csdnum0@gmail.com>
2024-09-18 04:22:28 +0900

    d

--- backup/lineTutorial/ecosystem.config_line.js
+++ backup/lineTutorial/ecosystem.config_line.js
@@ -8,6 +8,10 @@ module.exports = {
       wait_ready: true,
       listen_timeout: 50000,
       kill_timeout: 5000
+		//   ,
+		//   wait_ready: true, // Wait for the application to be in 'ready' state. The app must call `process.send('ready')` for PM2 to consider the instance ready. This ensures the application is fully ready before receiving requests.
+		//   listen_timeout: 50000, // Maximum time (ms) PM2 will wait for the 'ready' signal. Waits for 50,000ms (50 seconds) for the 'ready' signal; if not received, the application will be restarted.
+		//   kill_timeout: 5000  // Time (ms) PM2 will wait before forcefully killing a process. Allows 5,000ms (5 seconds) to finish existing requests before forcefully terminating the process.
     }
   ]
 }
